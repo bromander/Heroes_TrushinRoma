980612c4dfac98b06658710eb9ec3873052c3b68
bromander <116289268+bromander@users.noreply.github.com>
2024-09-17 21:59:17 +0700

    был добавлен def add_skill

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 from heroClasses.heroesClasses import MyHero
 
-gimli = MyHero("Гимли", "воин")
-print(gimli.add_exp(200))
-print(gimli.add_exp(300))
-print(gimli.add_exp(500))
+if __name__ == "__main__":
+    # Создаем героя с классом
+    gendalf = MyHero("Гендальф", "маг")
 
-print()
-
-legolas = MyHero("Леголас", "рейнджер")
-print(gimli.add_exp(500))
-print(gimli.add_exp(500))
-
-print()
-
-gendalf = MyHero("Гендальф", "маг")
-print(gendalf.add_exp())
+    # Добавляем опыт, чтобы повысить уровень и выбрать навыки
+    print(gendalf.add_exp(600))  # Достаточно опыта для уровня 2
